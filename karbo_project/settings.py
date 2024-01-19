99e20e8ff562c79183cf3b9293b5a0b33e48406a
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2024-01-19 15:47:23 +0200

    config for db

--- karbo_project/settings.py
+++ karbo_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'karbo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'django',
+        'PASSWORD': 'mysql5250',
+        'HOST': 'localhost',   # або '127.0.0.1'
+        'PORT': '3306',        # за замовчуванням порт MySQL - 3306
     }
 }
 
