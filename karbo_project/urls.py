21d6981c8bfc0960915d94af1c2769c3fd6121e5
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2024-01-20 12:11:04 +0200

    change urls

--- karbo_project/urls.py
+++ karbo_project/urls.py
@@ -20,6 +20,9 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('blog/', include('blog.urls')),
     path('', RedirectView.as_view(url='/blog/', permanent=True)),
+
+    path('api/', include('api.blog_api.urls'))
 ]
