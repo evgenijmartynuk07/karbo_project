141facfa78b1321232dc2c18f58ce6604f134225
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2024-01-20 12:29:03 +0200

    add link

--- api/blog_api/urls.py
+++ api/blog_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import PostViewSet
 
 urlpatterns = [
-    path('posts/', PostViewSet.as_view({"get": "list"}), name='post-list'),
+    path('posts/', PostViewSet.as_view({"get": "list"}), name='post-list-api'),
 ]
